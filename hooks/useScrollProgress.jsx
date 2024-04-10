f1fc7ed63abe97ffc3167029988fce7b0b5d4650
Milad Joodi <winbeta.com@gmail.com>
2024-04-11 01:09:01 +0330

    useScrollProgress

---
+++ hooks/useScrollProgress.jsx
@@ -0,0 +1,22 @@
+const { useState, useEffect } = require("react");
+
+const useScrollProgress = ()=>{
+    const [completion, setCompletion] = useState(0);
+
+    useEffect(()=>{
+        const updateScrollCompletion = ()=>{
+            const currentProgress = window.scrollY;
+            const scrollHeight = document.body.scrollHeight - window.innerHeight;
+
+            if(scrollHeight){
+                setCompletion(Number(currentProgress / scrollHeight).toFixed(2) * 100);
+            }
+        }
+        window.addEventListener('scroll', updateScrollCompletion);
+        return ()=>window.removeEventListener('scroll', updateScrollCompletion);
+    }, [])
+
+    return completion;
+}
+
+export default useScrollProgress;
